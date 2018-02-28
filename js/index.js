38a3148df464857044a35873ef9a8df16e097f49
AlxT <TsotosA@users.noreply.github.com>
2018-02-28 03:06:09 +0200

    Complete Remake

---
+++ js/index.js
@@ -0,0 +1,38 @@
+// SMALL SCREEN NAVIGATION
+$(document).ready(function() {
+  $(".button a").click(function() {
+    $(".overlay").fadeToggle(350);
+    $(this)
+      .toggleClass("btn-open")
+      .toggleClass("btn-close");
+  });
+  $(".overlay").on("click", function() {
+    $(".overlay").fadeToggle(350);
+    $(".button a")
+      .toggleClass("btn-open")
+      .toggleClass("btn-close");
+    open = false;
+  });
+});
+
+// NAVBAR
+$(document).ready(function() {
+  var scrollTop = 0;
+  $(window).scroll(function() {
+    scrollTop = $(window).scrollTop();
+    // $(".counter").html(scrollTop);
+
+    if (scrollTop >= 64) {
+      $("#navbar nav").addClass("scrolled-nav");
+    } else if (scrollTop < 65) {
+      $("#navbar nav").removeClass("scrolled-nav");
+    }
+  });
+});
+
+// TOOLTIP
+$(document).ready(function () {
+    $(".tooltip").tooltipster({
+        theme: "tooltipster-punk"
+    });
+});
